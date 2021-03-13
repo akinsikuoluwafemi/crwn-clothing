02e957a3141bd2c1207e67ca3073bf26427ad67f
Oluwafemi <akinsiku13@gmail.com>
2021-03-13 21:20:50 +0100

    update our menuitem to navigate withRouterHOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
 import './App.css';
-import HomePage from './homepage.component';
+import HomePage from './pages/homepage.component';
+import {Route, Switch, Link} from 'react-router-dom';
+
+
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+)
+
+const HatDetail = (props) => {
+  console.log(props)
+  return (
+    <div>
+      HatDetail
+    </div>
+  )
+}
+
 
 
 function App() {
   return (
-    <div >
-        <HomePage/>
+    <div>
+        <Switch>
+          <Route exact path="/" component={HomePage}/>
+          <Route exact path="/hats" component={HatsPage}/>
+          <Route path="/hats/:id" component={HatDetail}/>
+
+
+        </Switch>
     </div>
   );
 }
